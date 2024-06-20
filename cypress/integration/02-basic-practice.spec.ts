d6175392ba2883b89cf10b4fe7b6f1acc7246514
nathan-richards <nathinho@outlook.com>
2024-06-20 16:27:46 +0100

    Added filtering tests

--- cypress/integration/02-basic-practice.spec.ts
+++ cypress/integration/02-basic-practice.spec.ts
@@ -6,17 +6,41 @@ describe('Basic Practice', () => {
   });
 
   describe('Adding a new item', () => {
-    it('should put a new item on the page after clicking on "Add Item"', () => {});
+    const item = 'New Item';
 
-    it('should put a new item in the "Unpacked Items" list', () => {});
+    beforeEach(() => {
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('[data-test="add-item"]').click();
+    });
 
-    it('should put a new item as the last item in the "Unpacked Items" list', () => {});
+    it('should put a new item on the page after clicking on "Add Item"', () => {
+      cy.get('#jetsetter-application').should('contain.text', item);
+    });
+
+    it('should put a new item in the "Unpacked Items" list', () => {
+      cy.get('[data-test="items-unpacked"').should('contain.text', item);
+    });
+
+    it('should put a new item as the last item in the "Unpacked Items" list', () => {
+      cy.get('[data-test="items-unpacked"').children().last().should('contain.text', item);
+    });
   });
 
   describe('Filtering items', () => {
-    it('should show items that match whatever is in the filter field', () => {});
+    const filterTerm = 'Tooth';
 
-    it('should hide items that do not match whatever is in the filter field', () => {});
+    beforeEach(() => {
+      cy.get('[data-test="filter-items"]').type(filterTerm);
+    });
+
+    it('should show items that match whatever is in the filter field', () => {
+      cy.get('[data-test="filter-items"]').type(filterTerm);
+    });
+
+    it('should hide items that do not match whatever is in the filter field', () => {
+      cy.get('[data-test="items-unpacked"').contains('Hoodie').should('not.exist');
+      cy.get('[data-test="items-packed"').contains('Deoderant').should('not.exist');
+    });
   });
 
   describe('Removing items', () => {
