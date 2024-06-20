da4d20bce88b6d6a40c1f2af146bae12913c25aa
nathan-richards <nathinho@outlook.com>
2024-06-20 15:43:09 +0100

    Updated firts fight test

--- cypress/integration/01-first-flight.spec.ts
+++ cypress/integration/01-first-flight.spec.ts
@@ -5,11 +5,11 @@ describe('Create a New Item', () => {
     cy.visit('/jetsetter');
   });
 
-  it('should have a form', () => {
+  it('should have a form and contain add items', () => {
     cy.get('form').should('contain.text', 'Add Item');
   });
 
-  it('should have the words "Add Item"', () => {
-    cy.contains('Add Item');
+  it('should put stuff in an input field', () => {
+    cy.get('[data-test="new-item-input"]').type('New Item');
   });
 });
