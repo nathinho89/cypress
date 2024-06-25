1642b017d81a87d8ddf5b4f9d67170d4bca186b5
nathan-richards <nathinho@outlook.com>
2024-06-25 09:36:04 +0100

    Corrected error in echo chamber tests

--- cypress/integration/07-echo-chamber.spec.ts
+++ cypress/integration/07-echo-chamber.spec.ts
@@ -2,6 +2,7 @@
 
 describe('Initial Page', () => {
   beforeEach(() => {
+    cy.task('reset');
     cy.visit('/echo-chamber');
   });
 
@@ -13,24 +14,21 @@ describe('Initial Page', () => {
     cy.title().should('contain', 'Echo Chamber');
   });
 
+  it('should have a "Sign In" button', () => {
+    cy.get('[data-test="sign-in"]');
+  });
+
+  it('should have a "Sign Up" button', () => {
+    cy.get('[data-test="sign-up"]');
+  });
+
   it('should navigate to "/sign-in" when you click the "Sign In" button', () => {
     cy.get('[data-test="sign-in"]').click();
-    cy.location('pathname').should('eq', '/echo-chamber/sign-in');
+    cy.location('pathname').should('contain', 'sign-in');
   });
 
   it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {
     cy.get('[data-test="sign-up"]').click();
-    cy.location('pathname').should('eq', '/echo-chamber/sign-up');
-});
-
-describe('Sign Up', () => {
-  beforeEach(() => {
-    cy.visit('/echo-chamber/sign-up');
+    cy.location('pathname').should('contain', 'sign-up');
   });
-
-  it('should require an email', () => {});
-
-  it('should require that the email actually be an email address', () => {});
-
-  it('should require a password when the email is present', () => {});
 });
